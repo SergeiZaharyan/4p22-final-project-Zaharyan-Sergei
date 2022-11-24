1e52ed0f57a561aa256d1bc70bc0222a2b08cdf1
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2022-11-24 21:22:33 +0200

    update

--- my-app/src/components/MyComponent/CardPopup/CardPopup.js
+++ my-app/src/components/MyComponent/CardPopup/CardPopup.js
@@ -1,10 +1,16 @@
 import './CardPopup.css';
 function CardPopup(props) {
     return (
-        <div className={`CardPopupWrapper ${props.isOpened ? 'open' : 'close'}`} onClick={props.onPopupClose}>
-            
+        <div className={`CardPopupWrapper ${props.isOpened ? 'open' : 'close'}`}
+        onClick={e => {
+            e.stopPropagation();
+            props.onPopupClose();}}>
+        
             <div className="CardPopupConteiner">
-            <div className="CardPopupClose" onClick={props.onPopupClose}>X</div>
+            <div className="CardPopupClose" onClick={e => {
+                e.stopPropagation();
+                props.onPopupClose();
+            }}>X</div>
             <img className="CardPopupImg" alt={props.title} src={props.image} />
             <p className="CardPopupDescription">{props.description}</p>
             <h1 className="CardPopupPrice">$ {props.price}</h1>
